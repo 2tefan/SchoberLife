99203705eddbc77f8de4d0cfd540bbb4696efcd9
stefan <201310.stefan@gmail.com>
2019-05-07 11:23:13 +0200

    Added answers

--- app/src/main/java/at/schiebung/stefan/schober0021/Vars.java
+++ app/src/main/java/at/schiebung/stefan/schober0021/Vars.java
@@ -1,21 +1,23 @@
 package at.schiebung.stefan.schober0021;
 
-public class Vars
-{
-	static String preferences_name = "Schober by Stefan";
-	static String preferences_key = "ThisIsVerySecureAndNoOneWillGuessThis";
-	static int frage = 0;
-	static int[] anweresed;
+public class Vars {
+    static String preferences_name = "Schober by Stefan";
+    static String preferences_key = "ThisIsVerySecureAndNoOneWillGuessThis";
+    static int frage = 0;
+    static int[] anweresed;
 
-	static int reputation = 1000;
-	static int grade = 1000;
-	static int parents = 1000;
-	static int money = 1000;
+    static int reputation = 1000;
+    static int grade = 1000;
+    static int parents = 1000;
+    static int money = 1000;
 
-	//Beliebtheit - Note - Eltern - Geld
+    //Beliebtheit - Note - Eltern - Geld
 
-	static int[][][] change = {{{0, -50, -75, 0}, {-50, 50, 0, 0}, {-150, 100, 0, 0}, {25, -100, 0, 0}},
-			{{150, 0, 0, 0}, {0, -10, 0, 0}, {-25, 0, 0, 0}, {0, -150, 0, 0}},
-			{{-100, 0, 0, 0}, {10, 0, -50, 0}, {100, -50, -50, 0}, {-10000, -10000, -10000, -10000}},
-			{{5, -10, 0, 0}, {50, 0, 0, 0}, {0, -5, 0, 0}, {-25, 0, 0, 0}}};
+    static int[][][] change =
+            {{{0, -50, -75, 0}, {-50, 50, 0, 0}, {-150, 100, 0, 0}, {25, -100, 0, 0}},
+                    {{150, 0, 0, 0}, {0, -10, 0, 0}, {-25, 0, 0, 0}, {0, -150, 0, 0}},
+                    {{-100, 0, 0, 0}, {10, 0, -50, 0}, {100, -50, -50, 0}, {-10000, -10000, -10000, -10000}},
+                    {{5, -10, 0, 0}, {50, 0, 0, 0}, {0, -5, 0, 0}, {-25, 0, 0, 0}},
+                    {{0, -100, 0, 0}, {100, 0, 20, 0}, {-50, -50, 0, 0}, {-50, 250, 0, 0}},
+                    {{-300, 0, 0, 0}, {300, 50, 0, 0}, {-100, 0, 0, 200}, {-50, 0, 0, 0}}};
 }
