b63a5bfc9809645d51a44a5f01afe54c0f3f12e7
Stefan <201310.stefan@gmail.com>
2019-05-11 14:48:49 +0200

    Provide alternative bitmaps

--- app/src/main/java/at/schiebung/stefan/schober0021/Vars.java
+++ app/src/main/java/at/schiebung/stefan/schober0021/Vars.java
@@ -2,8 +2,6 @@ package at.schiebung.stefan.schober0021;
 
 class Vars
 {
-    static final String preferences_name = "Schober by Stefan";
-    static final String preferences_key = "ThisIsVerySecureAndNoOneWillGuessThis";
     static int lastQuestion = 0;
 
     static int reputation;
