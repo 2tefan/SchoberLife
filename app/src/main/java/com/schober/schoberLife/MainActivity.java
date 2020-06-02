6dd1d8d458206133bfb335cbe516324cbf226235
2tefan <2tefan@protonmail.com>
2020-06-02 10:03:51 +0200

    Updated build.gradle

--- app/src/main/java/com/schober/schoberLife/MainActivity.java
+++ app/src/main/java/com/schober/schoberLife/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.Random;
 
 import timber.log.Timber;
 
+@SuppressWarnings("unused")
 public class MainActivity extends AppCompatActivity
 {
     private       boolean   gameOver  = false;
